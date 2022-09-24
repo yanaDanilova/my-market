86be84bcc2daacb2ec79ad83f6422e6b3e6c46e6
yanad <yana.danilova1509@gmail.com>
2022-09-24 12:07:20 +0200

    Initial commit

---
+++ src/main/java/de/danilova/mymarket/MyMarketApplication.java
@@ -0,0 +1,13 @@
+package de.danilova.mymarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/danilova/mymarket/MyMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package de.danilova.mymarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
